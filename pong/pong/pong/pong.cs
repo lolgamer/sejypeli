a6f9f45bfe923d233203073463ef5ce1f1f96ba2
lolgamer <lollottelia@gmail.com>
2013-09-12 08:12:14 -0700

    joku vanha mailapeli vaiheessa 4 jeeeee

---
+++ pong/pong/pong/pong.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using Jypeli;
+using Jypeli.Assets;
+using Jypeli.Controls;
+using Jypeli.Effects;
+using Jypeli.Widgets;
+
+public class pong : PhysicsGame
+{
+    PhysicsObject pallo;
+
+    public override void Begin()
+    {
+        LuoKentta();
+        AloitaPeli();
+
+
+        
+        PhoneBackButton.Listen(ConfirmExit, "Lopeta peli");
+        Keyboard.Listen(Key.Escape, ButtonState.Pressed, ConfirmExit, "Lopeta peli");
+    }
+
+    void LuoKentta()
+    {
+        pallo = new PhysicsObject(40.0, 40.0);
+        pallo.Shape = Shape.Circle;
+        pallo.X = -200.0;
+        pallo.Y = 0.0;
+        pallo.Restitution = 1.0;
+        Add(pallo);
+     
+        PhysicsObject maila = PhysicsObject.CreateStaticObject(20.0, 100.0);
+        maila.Shape = Shape.Rectangle;
+        maila.X = Level.Left + 20.0;
+        maila.Y = 0.0;
+        maila.Restitution = 1.0;
+        Add(maila);
+
+        Level.CreateBorders(1.0, false);
+        Level.BackgroundColor = Color.Black;
+
+        Camera.ZoomToLevel();
+   
+    
+    }
+
+    void AloitaPeli()
+    {
+        Vector impulssi = new Vector(500.0, 0.0);
+        pallo.Hit(impulssi);
+    }
+
+}
